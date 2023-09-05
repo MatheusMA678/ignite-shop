6484809b0b64f85dbe928ae32ec48eec4641e249
MatheusMA678 <matheusma2510@hotmail.com>
2023-09-05 01:11:44 -0400

    fix: 405 bug

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -4,17 +4,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { priceId } = req.body
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" })
-  }
+  // if (req.method !== "POST") {
+  //   return res.status(405).json({ error: "Method not allowed" })
+  // }
 
   if (!priceId) {
     return res.status(404).json({ error: "Price id not found" })
   }
 
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.NEXT_URL}`,
+    success_url: `${process.env.BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.BASE_URL}`,
     mode: "payment",
     line_items: [
       {
